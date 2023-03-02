9f34d7630926babdad33687cd49e3c68934b69f0
Bharath Variar <bharathvariar@gmail.com>
2023-03-02 11:54:04 +0530

    Wrote a lot of comments in .hpp file, but couldn't run doxygen

--- DCEL.cpp
+++ DCEL.cpp
@@ -1,20 +1,19 @@
 #include "DCEL.hpp"
 
-Face::Face(int n)
-{
+/// @brief 
+/// @param n 
+Face::Face(int n) {
     id = n;
 }
-/// @brief Initialize an edge to contain to halfedges
-/// @param a
-/// @param b
-Edge::Edge(HalfEdge *a, HalfEdge *b)
-{
+/// @brief Initialize an edge to contain two half-edges
+/// @param a First half-edge
+/// @param b Second half-edge
+Edge::Edge(HalfEdge *a, HalfEdge *b) {
     edge.first = a;
     edge.second = b;
 }
 /// @brief initialzes the DCEL to connect one exterior face
-DCEL::DCEL()
-{
+DCEL::DCEL() {
     faces.push_back(new Face(0));
 }
 
@@ -23,31 +22,24 @@ DCEL::DCEL()
 /// @param v2 Vertex to which edge has to be retuned.
 /// @param currFace currFace The face that needs to be split by the added edge.
 /// @return The HalfEdge from v1.
-HalfEdge *DCEL::addEdge(Vertex *&v1, Vertex *&v2, Face *&currFace)
-{
+HalfEdge *DCEL::addEdge(Vertex *&v1, Vertex *&v2, Face *&currFace) {
     int fid = currFace->id;
     HalfEdge *fromV1 = new HalfEdge();
     HalfEdge *fromV2 = new HalfEdge();
     HalfEdge *halfV1, *halfV2;
-    for (int i = 0; i < edges.size(); i++)
-    {
-        if (edges[i]->edge.first->f->id == fid && edges[i]->edge.first->v == v1)
-        {
+    for (int i = 0; i < edges.size(); i++) {
+        if (edges[i]->edge.first->f->id == fid && edges[i]->edge.first->v == v1) {
             halfV1 = *&edges[i]->edge.first;
         }
-        if (edges[i]->edge.second->f->id == fid && edges[i]->edge.second->v == v1)
-        {
+        if (edges[i]->edge.second->f->id == fid && edges[i]->edge.second->v == v1) {
             halfV1 = *&edges[i]->edge.second;
         }
     }
-    for (int i = 0; i < edges.size(); i++)
-    {
-        if (edges[i]->edge.first->f->id == fid && edges[i]->edge.first->v == v2)
-        {
+    for (int i = 0; i < edges.size(); i++) {
+        if (edges[i]->edge.first->f->id == fid && edges[i]->edge.first->v == v2) {
             halfV2 = *&edges[i]->edge.first;
         }
-        if (edges[i]->edge.second->f->id == fid && edges[i]->edge.second->v == v2)
-        {
+        if (edges[i]->edge.second->f->id == fid && edges[i]->edge.second->v == v2) {
             halfV2 = *&edges[i]->edge.second;
         }
     }
@@ -73,8 +65,7 @@ HalfEdge *DCEL::addEdge(Vertex *&v1, Vertex *&v2, Face *&currFace)
     currFace->he = fromV1;
     edges.push_back(new Edge(fromV1, fromV2));
     temp = temp->next;
-    while (temp != fromV2)
-    {
+    while (temp != fromV2) {
         HalfEdge *temp2 = &*temp;
         temp2->f = face;
         temp = temp->next;
@@ -85,8 +76,7 @@ HalfEdge *DCEL::addEdge(Vertex *&v1, Vertex *&v2, Face *&currFace)
 }
 /// @brief
 /// @param h
-void DCEL::removeEdge(HalfEdge *&h)
-{
+void DCEL::removeEdge(HalfEdge *&h) {
     HalfEdge *t = h->twin;
     HalfEdge *h1_prev = h->prev;
     HalfEdge *h1_next = t->next;
@@ -101,16 +91,14 @@ void DCEL::removeEdge(HalfEdge *&h)
     HalfEdge *temp = h1_next;
     temp->f = f1;
     temp = temp->next;
-    while (temp != h1_next)
-    {
+    while (temp != h1_next) {
         temp->f = f1;
         temp = temp->next;
     }
     f1->he = h1_next;
     free(h);
     vector<Face *> f;
-    for (int i = 0; i < faces.size(); i++)
-    {
+    for (int i = 0; i < faces.size(); i++) {
         if (faces[i] != f2)
             f.push_back(faces[i]);
     }
@@ -119,8 +107,7 @@ void DCEL::removeEdge(HalfEdge *&h)
 /// @brief
 /// @param x
 /// @param y
-void DCEL::addVertex(double x, double y)
-{
+void DCEL::addVertex(double x, double y) {
     Vertex *v = new Vertex();
     v->x = x;
     v->y = y;
@@ -128,27 +115,23 @@ void DCEL::addVertex(double x, double y)
     vertices.push_back(v);
 }
 
-void DCEL::addFace(int id)
-{
+void DCEL::addFace(int id) {
     Face *f = new Face(id);
     faces.push_back(f);
 }
 
-void DCEL::createPolygon(char *file)
-{
+void DCEL::createPolygon(char *file) {
     freopen(file, "r", stdin);
     int n;
     cin >> n;
     double x, y;
-    while (n--)
-    {
+    while (n--) {
         cin >> x >> y;
         addVertex(x, y);
     }
     n = vertices.size();
     addFace(1);
-    for (int i = 0; i < vertices.size(); i++)
-    {
+    for (int i = 0; i < vertices.size(); i++) {
         HalfEdge *he = new HalfEdge();
         HalfEdge *t = new HalfEdge();
         he->v = vertices[i];
@@ -161,53 +144,44 @@ void DCEL::createPolygon(char *file)
         faces[0]->he = t;
         edges.push_back(new Edge(he, t));
     }
-    for (int i = 0; i < edges.size(); i++)
-    {
+    for (int i = 0; i < edges.size(); i++) {
         edges[i]->edge.first->next = edges[(i + 1) % n]->edge.first;
         edges[i]->edge.first->prev = edges[(i - 1 + n) % n]->edge.first;
     }
-    for (int i = 0; i < edges.size(); i++)
-    {
+    for (int i = 0; i < edges.size(); i++) {
         edges[i]->edge.second->next = edges[(i - 1 + n) % n]->edge.second;
         edges[i]->edge.second->prev = edges[(i + 1) % n]->edge.second;
     }
 }
 
-void DCEL::print_()
-{
-    for (int i = 0; i < faces.size(); i++)
-    {
+void DCEL::print_() {
+    for (int i = 0; i < faces.size(); i++) {
         HalfEdge *temp = faces[i]->he;
         cout << "Face: " << faces[i]->id << '\n';
         cout << temp->v->index << " ";
         temp = temp->next;
-        while (temp != faces[i]->he)
-        {
+        while (temp != faces[i]->he) {
             cout << temp->v->index << " ";
             temp = temp->next;
         }
         cout << '\n';
     }
 }
-void DCEL::outputPolygon(char *file)
-{
+void DCEL::outputPolygon(char *file) {
     freopen(file, "w+", stdout);
-    for (int i = 0; i < faces.size(); i++)
-    {
+    for (int i = 0; i < faces.size(); i++) {
         HalfEdge *temp = faces[i]->he;
         // cout << "Face: " << faces[i]->id << '\n';
         cout << temp->v->x << " " << temp->v->y << '\n';
         temp = temp->next;
-        while (temp != faces[i]->he)
-        {
+        while (temp != faces[i]->he) {
             cout << temp->v->x << " " << temp->v->y << '\n';
             temp = temp->next;
         }
         cout << "$\n";
     }
 }
-DCEL::~DCEL()
-{
+DCEL::~DCEL() {
     vertices.clear();
     edges.clear();
     faces.clear();
